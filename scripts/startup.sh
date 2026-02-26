3166fd5fca5c95ad6975d20985258dad9b89f9fe
Jon <jon.nguyen7@protonmail.com>
2026-02-26 06:06:46 -0600

    startup script

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -3,7 +3,7 @@ foot --server &
 syncthing &
 waybar &
 /usr/lib/soteria-polkit/soteria &
-swaybg -i /home/hacky/Pictures/Wallpapers/Train (catpu).png &
+swaybg -i /home/hacky/Pictures/Wallpapers/pixel_road.png &
 gnome-keyring-daemon --start --components=secrets
 
 exec dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP=wlroots
