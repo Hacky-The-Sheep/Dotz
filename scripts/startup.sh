5ca699a212ef6d277407ea4965bf588aa7a1df71
Jon <jon.nguyen7@protonmail.com>
2026-04-06 21:23:08 -0500

    pipewire

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -5,5 +5,6 @@ dbus-update-activation-environment --all
 syncthing &
 # waybar &
 # swww-daemon &
+pipewire &
 gnome-keyring-daemon --start --components=secrets &
 exec dbus-run-session niri --session
